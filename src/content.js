81366bcd7b90a91a0b4743e52ae54d07b07646ba
Casmir Chukwuma <casmirakachi@gmail.com>
2021-11-29 16:04:42 +0200

    added content

--- src/content.js
+++ src/content.js
@@ -11,12 +11,6 @@ const content = [
     body: "I enjoy creating and buidling software solutions from frontend design to backend configurations and best practices. I like to code and believe people should do things that make them happy.",
     image: "illustration-your-users.svg",
   },
-  // {
-  //   id: 3,
-  //   title: "Contact",
-  //   body: "You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
-  //   image: "illustration-grow-together.svg",
-  // },
 ];
 
 export default content;
